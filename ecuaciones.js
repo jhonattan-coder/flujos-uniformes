07d3df9e47b857a1d61e0dfd8f1b8806580c3709
jhonattan-coder <jhonattanalpiso@gmail.com>
2020-08-18 12:04:35 -0430

    pulldecambiosengithubycalculosdelflujocritico

--- ecuaciones.js
+++ ecuaciones.js
@@ -127,7 +127,7 @@ function tiran_unif_canal_rect(){
 	p.textContent = 'Asegurese de ingresar datos válidos';
 	}
 }
-//////////////////////////////// Ecuaciones del tirante uniforme en canal rectangular
+//////////////////////////////// Ecuaciones del tirante uniforme en canal circular
 function tiran_unif_canal_circ(){
 	var d = parseFloat(document.getElementById("d6").value);
 	var sf = parseFloat(document.getElementById("sf6").value);
@@ -135,9 +135,9 @@ function tiran_unif_canal_circ(){
 	var q = parseFloat(document.getElementById("q6").value);
 	var p = document.getElementById("p6");
 	if (d>0 && sf>=0 && n>0 && q>0){
-	var y0 = 1;
+	var y0 = 0.9*d;
 	
-	for (i=1; i<50; i++){
+	for (i=1; i<8; i++){
 	var a0=2*Math.acos((1-(2*y0/d)));
 	var A = 0.5*Math.pow(d,2)*(a0-Math.sin(a0));
 	var T = d*Math.sin(a0/2);
@@ -189,7 +189,7 @@ function tiran_unif_canal_trap(){
 	p.textContent = 'Asegurese de ingresar datos válidos';
 	}
 }
-//////////////////////////////// Ecuaciones del tirante uniforme en canal trapezoidal
+//////////////////////////////// Ecuaciones del tirante uniforme en canal triangular
 function tiran_unif_canal_trian(){
 	var z = parseFloat(document.getElementById("z8").value);
 	var sf = parseFloat(document.getElementById("sf8").value);
@@ -214,6 +214,126 @@ function tiran_unif_canal_trian(){
 	}
 
 	else if (b<=0 || sf<0 || n<=0 || q<=0){
+	p.textContent = 'Asegurese de ingresar datos válidos';
+	}
+}
+//////////////////////////////// Ecuaciones del tirante crítico en canal rectangular
+function tiran_crit_canal_rect(){
+	var b = parseFloat(document.getElementById("b9").value);
+	var q = parseFloat(document.getElementById("q9").value);
+	var p = document.getElementById("p9");
+	if (b>0 && q>0){
+	var y0 = 1;
+	var y1 = 5;
+	
+	for (i=1; i<8; i++){
+	var A0 = y0 * b;
+	var A1 = y1 * b;
+	var D0 = y0;
+	var D1 = y1;
+	var V0 = q/A0;
+	var V1 = q/A1;
+	var f0 = criterio(V0,D0);
+	var f1 = criterio(V1,D1);;
+	var y2 = y1-(((f1)*(y0-y1))/(f0-f1));
+	y0=y1;
+	y1=y2;
+		}
+	p.textContent = 'Tirante critico: ' + y1 + ' m';
+	}
+
+	else if (b<=0 || q<=0){
+	p.textContent = 'Asegurese de ingresar datos válidos';
+	}
+}
+//////////////////////////////// Ecuaciones del tirante crítico en canal circular
+function tiran_crit_canal_circ(){
+	var d = parseFloat(document.getElementById("d10").value);
+	var q = parseFloat(document.getElementById("q10").value);
+	var p = document.getElementById("p10");
+	if (d>0 && q>0){
+	var y0 = 0.9*d;
+	var y1 = 0.1*d;
+	
+	for (i=1; i<8; i++){
+	var ang0=2*Math.acos(1-(2*y0/d));
+	var ang1=2*Math.acos(1-(2*y1/d));
+	var A0 = 0.5*d*d*0.25*(ang0-Math.sin(ang0));
+	var A1 = 0.5*d*d*0.25*(ang1-Math.sin(ang1));
+	var D0 = A0/(Math.sin(0.5*ang0)*d);
+	var D1 = A1/(Math.sin(0.5*ang1)*d);
+	var V0 = q/A0;
+	var V1 = q/A1;
+	var f0 = criterio(V0,D0);
+	var f1 = criterio(V1,D1);
+	var ang2 = ang1-(((f1)*(ang0-ang1))/(f0-f1));
+	y2=0.5*d*(1-(Math.cos(ang2/2)));
+	y0=y1;
+	y1=y2;
+		}
+	p.textContent = 'Tirante critico: ' + y1 + ' m';
+	}
+
+	else if (b<=0 || q<=0){
+	p.textContent = 'Asegurese de ingresar datos válidos';
+	}
+}
+//////////////////////////////// Ecuaciones del tirante crítico en canal trapezoidal
+function tiran_crit_canal_trap(){
+	var b = parseFloat(document.getElementById("b11").value);
+	var z = parseFloat(document.getElementById("z11").value);
+	var q = parseFloat(document.getElementById("q11").value);
+	var p = document.getElementById("p11");
+	if (b>0 && z>0 && q>0){
+	var y0 = 2*b;
+	var y1 = 0.5*b;
+	
+	for (i=1; i<8; i++){
+	var A0 = y0 * (b + z*y0);
+	var A1 = y1 * (b + z*y1);
+	var D0 = A0/(b+2*z*y0);
+	var D1 = A1/(b+2*z*y1);
+	var V0 = q/A0;
+	var V1 = q/A1;
+	var f0 = criterio(V0,D0);
+	var f1 = criterio(V1,D1);;
+	var y2 = y1-(((f1)*(y0-y1))/(f0-f1));
+	y0=y1;
+	y1=y2;
+		}
+	p.textContent = 'Tirante critico: ' + y1 + ' m';
+	}
+
+	else if (b<=0 || z<=0 || q<=0){
+	p.textContent = 'Asegurese de ingresar datos válidos';
+	}
+}
+//////////////////////////////// Ecuaciones del tirante crítico en canal triangular
+function tiran_crit_canal_trian(){
+	var z = parseFloat(document.getElementById("z12").value);
+	var q = parseFloat(document.getElementById("q12").value);
+	var p = document.getElementById("p12");
+	if (z>0 && q>0){
+	var y0 = 1;
+	var y1 = 4;
+	
+	for (i=1; i<8; i++){
+	var A0 = y0*z*y0;
+	var A1 = y1*z*y1;
+	var D0 = A0/(2*z*y0);
+	var D1 = A1/(2*z*y1);
+	var V0 = q/A0;
+	var V1 = q/A1;
+	var f0 = criterio(V0,D0);
+	var f1 = criterio(V1,D1);;
+	var y2 = y1-(((f1)*(y0-y1))/(f0-f1));
+	y0=y1;
+	y1=y2;
+		}
+	p.textContent = 'Tirante critico: ' + y1 + ' m';
+	}
+
+	else if (z<=0 || q<=0){
 	p.textContent = 'Asegurese de ingresar datos válidos';
 	}
 }
@@ -245,6 +365,9 @@ function derivadaManningTrap (R,n,T,z){
 	var Pot_RH = Math.pow(R,2/3);
 	var inv_n = 1/n;
 	return -1 * Pot_RH * inv_n * ((5*T-4*R*z)/3);
+	}
+function criterio (V,D){
+	return ((V*V)/(19.62))-0.5*D;
 	}
 //////////////////////////////// -------------------------------------------------------- ----------  --------------------------
 function openTab(id, elmnt, color, tablink, tabcontent, defaultOpen) {
